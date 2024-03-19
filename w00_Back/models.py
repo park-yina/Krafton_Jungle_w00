ac7b75c65aa5e4cdf13c4bb2cc8c35233a8946e8
JunHyeokDev <fixme1537@gmail.com>
2024-03-19 23:54:36 +0900

    jwt어려워
    pip install -r requirements.txt
    
    하면 자동으로 라이브러리 설치됩니다

---
+++ w00_Back/models.py
@@ -0,0 +1,16 @@
+# models.py
+import PIL.Image
+import base64
+from io import BytesIO
+import google.generativeai as genai
+
+imgModel = genai.GenerativeModel('gemini-pro-vision')
+
+def imgToStoryData(path):
+    img = PIL.Image.open(path)
+    response = imgModel.generate_content(["해당 그림을 바탕으로 한 문장의 짧고 재미있는 스토리 만들어줘.", img])
+    response.resolve()
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    return {'img_src': img_str, 'text': response.text}
