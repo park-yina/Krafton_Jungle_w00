6a02aea606a808abcc0a79a6e09971e55592597f
Lee Kyuhong <6puritans9@gmail.com>
2024-03-19 21:52:36 +0900

    뒤로가기 작성

--- w00_Front/src/app.js
+++ w00_Front/src/app.js
@@ -1,3 +1,14 @@
+// HTML ELEMENTS
+const goBackBtn = document.querySelector("#goBackBtn");
+const checkOthers = document.querySelector("#checkOthers");
+
+// EVENTLISTENERS
+goBackBtn.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
+checkOthers.addEventListener("click", () => {}); // 채워 넣기
+
+// Functions
 class File {
   static upload(uploaderEl) {
     uploaderEl.addEventListener("change", (event) => {
@@ -8,5 +19,4 @@ class File {
 }
 
 const fileInput = document.querySelector("#fileInput");
-
 fileInput.addEventListener("change", File.upload(fileInput));
